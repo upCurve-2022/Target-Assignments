0a6c2daa2527c8dba52515a8986f719a8624b3b8
Keerthi Reddy <100691785+keerthii2332@users.noreply.github.com>
2022-03-31 13:00:27 +0530

    Add files via upload

---
+++ stringassignment/Emptystring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Emptystring
+{
+  public static void main(String args[])
+    {
+        String str="";
+         if(str.isEmpty())
+           System.out.println("String is empty");
+         else
+           System.out.println("String is not empty");
+     }
+}
