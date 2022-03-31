ce866118c0e1fbdf10b2e9a533a03e633d70a1b3
Keerthi Reddy <100691785+keerthii2332@users.noreply.github.com>
2022-03-31 19:58:59 +0530

    Create concatstrings.java

---
+++ JavaChallenges/concatstrings.java
@@ -0,0 +1,10 @@
+public class concatstrings
+{
+    public static void main(String[] args) 
+    {
+        String s1 = "Keerthi";
+        String s2 = "Reddy";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+    }
+}
