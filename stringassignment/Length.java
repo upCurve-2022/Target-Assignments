4487f61fbb24e1237c16940c3b8b1c39c0c363c7
Keerthi Reddy <100691785+keerthii2332@users.noreply.github.com>
2022-03-31 13:01:48 +0530

    Add files via upload

---
+++ stringassignment/Length.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Length{
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        String str = in.next();
+        System.out.println("The size of "+ "the String is "+ str.length());
+    }
+}
