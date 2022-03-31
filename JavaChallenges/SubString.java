953e2ed0a588e830a5c3a7e15567cde06da62aa1
Keerthi Reddy <100691785+keerthii2332@users.noreply.github.com>
2022-04-01 00:13:53 +0530

    Add files via upload

---
+++ JavaChallenges/SubString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SubString
+{
+   public static void main(String args[])
+       {
+           Scanner in=new Scanner(System.in);
+            String str1=in.nextLine();
+            String str2=in.nextLine();
+            System.out.println(str2.contains(str1));
+        }
+}
