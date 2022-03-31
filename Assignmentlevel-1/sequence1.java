c358ff4c1196df68140b13dd27c4f2ac5c3c3600
Keerthi Reddy <100691785+keerthii2332@users.noreply.github.com>
2022-03-31 15:22:57 +0530

    Create sequence1.java

---
+++ Assignmentlevel-1/sequence1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main
+{
+    public static void sequence1(int n)
+    {
+        for(int i=1; i<n; i++)
+    
+            {
+                System.out.print(i*i*4+" ");
+            }
+            
+   }
+
+    public static void main(String args[])
+    {
+        int n = 6;
+        sequence(n);
+    }
+}
