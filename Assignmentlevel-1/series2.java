ef69b96b9b2d2e5bd49ab30187cefb46933452ef
Keerthi Reddy <100691785+keerthii2332@users.noreply.github.com>
2022-03-31 18:44:54 +0530

    Create series2.java

---
+++ Assignmentlevel-1/series2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class series2
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N:");
+        int N = sc.nextInt();
+        int first = 1;
+        int second = 1;
+        int next;
+        if(N>2)
+        {
+            for (int i=0;i<N;i++)
+            {
+                System.out.println();
+            }
+        }
+
+    }
+}
